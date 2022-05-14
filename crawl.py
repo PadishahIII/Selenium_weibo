dde730d9bba8784ce13aeee241703eb8dfa8fc76
PadishahIII <350717997@qq.com>
2022-05-14 22:38:04 +0800

    拼接url方式的最后一次更新

--- crawl.py
+++ crawl.py
@@ -14,8 +14,8 @@ def scrapy():
     max_page = 1
 
     page = 1
-    current_page = 1
-    since_id = '4738962205704901'
+    current_page = 2
+    since_id = '4738962205704901'  #4738962205704901
 
     while (page <= max_page):
         target = "https://weibo.com/p/100808ec2f8f02483cbf2206505d27f9ffb3c1/super_index?pids=Pl_Core_MixedFeed__262&current_page={0}&since_id={1}&page={2}&ajaxpagelet=1&ajaxpagelet_v6=1&__ref=%2Fp%2F100808ec2f8f02483cbf2206505d27f9ffb3c1%2Fsuper_index%3Fcurrent_page%3D12%26since_id%3D4635626404844202%26page%3D1%23Pl_Core_MixedFeed__262&_t=FM_165252921938915".format(
@@ -34,12 +34,12 @@ def scrapy():
         data += res_doc
 
         #翻页
-        def getWholePage(current_page_ini):
+        def getWholePage(current_page_ini, page):
             data = ''
             rnd = "1652528312609"  #递增
             current_page = current_page_ini
             since_id = ['4762965007931076',
-                        '4759948456364531']  #4759948456364531
+                        '4759948456364531']  #4762965007931076,4759948456364531
             since_id_index = 0
             cnt = 0
             while (cnt < len(since_id)):
@@ -48,8 +48,9 @@ def scrapy():
                     pagebar = '0'
                 target = "https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100808&current_page=" + str(
                     current_page
-                ) + "&since_id=" + since_id[
-                    since_id_index] + "&page=1&pagebar=" + pagebar + "&tab=super_index&pl_name=Pl_Core_MixedFeed__262&id=100808ec2f8f02483cbf2206505d27f9ffb3c1&script_uri=/p/100808ec2f8f02483cbf2206505d27f9ffb3c1/super_index&feed_type=1&pre_page=1&domain_op=100808&__rnd=" + rnd
+                ) + "&since_id=" + since_id[since_id_index] + "&page=" + str(
+                    page
+                ) + "&pagebar=" + pagebar + "&tab=super_index&pl_name=Pl_Core_MixedFeed__262&id=100808ec2f8f02483cbf2206505d27f9ffb3c1&script_uri=/p/100808ec2f8f02483cbf2206505d27f9ffb3c1/super_index&feed_type=1&pre_page=1&domain_op=100808&__rnd=" + rnd
                 res = request.Request(target)
                 res.add_header(
                     "Cookie",
@@ -61,7 +62,7 @@ def scrapy():
                 res_doc = res_doc.decode('unicode_escape')  #指定编码方式
                 if (since_id_index == 0):
                     print("len:" + str(len(res_doc)))  #131123
-                    print(int(since_id[0]) - int(since_id[1]))  #3016551566545
+                    #print(int(since_id[0]) - int(since_id[1]))  #3016551566545
 
                 data += res_doc
                 rnd = str(int(rnd) + 100000)
@@ -70,8 +71,8 @@ def scrapy():
                 since_id_index += 1
             return data, current_page
 
+        data_, current_page = getWholePage(current_page, page)
         page += 1
-        data_, current_page = getWholePage(current_page)
         data += data_
 
     return data
@@ -98,10 +99,12 @@ def getText(html):
 
 if __name__ == "__main__":
     file = open("test", "w", encoding="utf8", errors='replace')
+    out = open("res.html", "w", encoding="utf8", errors='replace')
     #爬取网页
     res_doc = scrapy()
     #预处理及提取
     html = str(res_doc)
+    out.write(html)
     html_res = preprocess(html)
     list = getText(html_res)
     print(len(list))
@@ -111,5 +114,6 @@ if __name__ == "__main__":
         index += 1
         print(i, file=file)
         print('\n', file=file)
-
+    file.close()
+    out.close()
     #print(html_res)
