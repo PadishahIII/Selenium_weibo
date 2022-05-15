651607106b5992e79b1d232fd474483843350117
PadishahIII <350717997@qq.com>
2022-05-15 15:06:12 +0800

    period update

--- crawl.py
+++ crawl.py
@@ -5,6 +5,7 @@ import re
 from bs4 import BeautifulSoup
 import urllib.request as request
 import json
+from matplotlib.font_manager import json_dump
 
 from pymysql import NULL
 
@@ -64,7 +65,7 @@ def scrapy(page_index):
             res_doc = response.read()
             res_doc = res_doc.decode('unicode_escape',
                                      errors='replace')  #指定编码方式
-            #res_doc = jsonDecode(res_doc)
+            res_doc = getData(res_doc)
             if (since_id_index == 0):
                 print("len:" + str(len(res_doc)))  #131123
                 #print(int(since_id[0]) - int(since_id[1]))  #3016551566545
@@ -89,18 +90,13 @@ def scrapy(page_index):
     return data
 
 
-def jsonDecode(jsonstr):
+#从json字符串中提取data字段
+def getData(jsonstr):
     jsonlist = list(jsonstr)
     index = jsonstr.find('"data":')
     jsonlist[index + len('"data":')]
-    for i in range(index + len('"data":') + 1,
-                   len(jsonstr) - 2):  #将data中的双引号变成单引号，否则jsonDecode异常
-        if (jsonlist[i] == '"'):
-            jsonlist[i] = "'"
-    jsonstr = ''.join(jsonlist)
-    json_obj = json.loads(jsonstr, strict=False)
-    if (json_obj != NULL and 'data' in json_obj.keys()):
-        return json_obj['data'].replace("'", '"')
+    datastr = ''.join(jsonlist[index + len('"data":') + 1:len(jsonlist) - 2])
+    return datastr.replace(r"\/", "/")
 
 
 def getUrlList(html):
@@ -114,13 +110,63 @@ def getUrlList(html):
     return list(url_list)
 
 
-def preprocess(html):
+#对整页的html预处理
+def whole_preprocess(html):
+    html_res = re.sub("<a.*?a>", '', html, 0)  #去掉链接
+    #html_res = re.sub(r"\s", '', html_res, 0)  #去掉表情
+    file = open("json.html", "w", encoding="utf8", errors="replace")
+
+    #将script中的html代码提取出来,由于不需要utf8解码,html中的双引号已经是\",可以直接json解码
+    json_list = re.findall("<script>FM.view\((.+)\);?</script>", html_res)
+    for json_str in json_list:
+        json_obj = json.loads(json_str, strict=False)
+        if ("html" in json_obj.keys()):
+            html_res += json_obj.get('html')
+            #print(json_obj.get('html'), file=file)
+    print(html_res, file=file)
+    file.close()
+    return html_res
+
+
+#截取包含正文的div
+def text_preprocess(html):
     rst = re.compile('<div class=.{0,4}WB_text W_f14.*?div>')  #非贪婪模式
     result = rst.findall(html)
     html_res = "\n".join(result)
     html_res = html_res.replace('\\', '')
     html_res = re.sub("<a.*?a>", '', html_res, 0)  #去掉链接
     return html_res
+
+
+def getAllData(html):
+    WB_detail_bf = BeautifulSoup(html, features="html.parser")
+    WB_detail = WB_detail_bf.find_all("div", class_="WB_detail")
+    nickname_list = []  #昵称
+    date_list = []  #帖子的时间
+    text_list = []  #正文
+
+    for i in WB_detail:
+        WB_info_bf = BeautifulSoup(str(i))
+
+        WB_info_list = WB_info_bf.find_all("div", class_="WB_info")
+        WB_from_S_txt2_list = WB_info_bf.find_all("div",
+                                                  class_="WB_from S_txt2")
+        WB_text_W_f14_list = WB_info_bf.find_all("div", class_="WB_text W_f14")
+
+        a_nickname_bf = BeautifulSoup(str(WB_info_list[0]))
+        a_date_bf = BeautifulSoup(str(WB_from_S_txt2_list[0]))
+
+        a_nickname_list = a_nickname_bf.find_all("a")
+        a_date_list = a_date_bf.find_all("a")
+
+        if (len(a_nickname_list) > 0):
+            nickname_list.append(a_nickname_list[0].get("nick-name"))
+        if (len(a_date_list) > 0):
+            date_list.append(a_date_list[0].get("title"))
+        if (len(WB_text_W_f14_list) > 0):
+            text_list.append(WB_text_W_f14_list[0].text.replace(' ', ''))
+
+    return nickname_list, date_list, text_list
 
 
 #获取所有帖子正文
@@ -139,17 +185,34 @@ if __name__ == "__main__":
     #爬取网页
     UrlList = []
     res_doc, UrlList = scrapy(1)
+    html_res = whole_preprocess(res_doc)
+    nickname_list, date_list, text_list = getAllData(html_res)
+
+    for i in range(0, len(nickname_list)):
+        file.write(str(i) + ".  " + date_list[i] + "\n")
+        file.write(nickname_list[i] + ":" + text_list[i] + "\n")
+        file.write("\n")
+    for i in range(len(nickname_list), len(text_list)):
+        print(i, file=file)
+        print(text_list[i], file=file)
+        file.write("\n")
+
+    print(len(text_list))
+
     print(len(UrlList))
     for i in UrlList:
         print(i)
+
     page = 2
-    #while(page<)
+
     #预处理及提取
     html = str(res_doc)
     out.write(html)
-    html_res = preprocess(html)
+    html_res = text_preprocess(html)
     list = getText(html_res)
+
     print(len(list))
+
     index = 1
     for i in list:
         print(index, file=file)
